d7cc5f1f56c4f9ef7217f767112bd50928638742
lukemcdonnell <luke@hutsix.com.au>
2017-07-29 15:03:47 +0930

    migration

---
+++ app/DoctrineMigrations/Version20170729053634.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170729053634 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE volunteer_enrolment ADD can_do_it TINYINT(1) DEFAULT NULL, ADD can_do_it_date_time DATETIME DEFAULT NULL, ADD confirmed TINYINT(1) DEFAULT NULL, ADD confirmed_date_time DATETIME DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE volunteer_enrolment DROP can_do_it, DROP can_do_it_date_time, DROP confirmed, DROP confirmed_date_time');
+    }
+}
