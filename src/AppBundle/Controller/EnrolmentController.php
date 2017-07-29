59e128bb6c4142375db04684f361327aa217f9a5
lukemcdonnell <luke@hutsix.com.au>
2017-07-29 15:27:24 +0930

    Tweaks

--- src/AppBundle/Controller/EnrolmentController.php
+++ src/AppBundle/Controller/EnrolmentController.php
@@ -3,19 +3,40 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Emergency;
+use AppBundle\Entity\VolunteerEnrolment;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class EnrolmentController extends Controller
+class EnrolmentController extends AppController
 {
     /**
      * @Route("/list/{id}")
      */
     public function listAction(Emergency $emergency)
     {
+        $volunteers = $this->em()->getRepository("AppBundle:Volunteer")->findUnenrolledBySkills($emergency, $emergency->getSkills());
+
+        foreach ($volunteers as $volunteer) {
+            $enrolment = new VolunteerEnrolment(
+                $emergency,
+                $volunteer
+            );
+            $this->em()->persist($enrolment);
+        }
+
+        $this->em()->flush();
+
         return $this->render(':enrolment:list.html.twig', [
             "emergency" => $emergency,
         ]);
+    }
+
+    /**
+     * @Route("/confirm-enrolment/{id}")
+     */
+    public function confirmAction(VolunteerEnrolment $enrolment)
+    {
+        $enrolment->setConfirmed(true);
     }
 
 }
